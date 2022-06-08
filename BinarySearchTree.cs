be8c4eae2de5cf8ac4cfc89ea787f9834bd66a52
tusharvidwans99 <tusharvidwans99@gmail.com>
2022-06-08 06:54:36 +0530

    [Tushar Vidwans] Adding ability to create a BST by adding 56 as root node and then 30 & 70

---
+++ BinarySearchTree.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinarySearchTree_Day15
+{
+    public class BinarySearchTree<T> where T: IComparable<T>
+    {
+
+        public T NodeData { get; set; }
+        public BinarySearchTree<T> LeftTree { get; set; }
+        public BinarySearchTree<T> RightTree { get; set; }
+
+        public BinarySearchTree(T nodeData)
+        {
+            this.NodeData = nodeData;
+            this.LeftTree = null;
+            this.RightTree = null;
+        }
+        int leftCount =0, rightCount = 0;
+        bool result = false;
+
+        public void Insert(T item)
+        {
+            T cureentNodeValue = this.NodeData;
+            if ((cureentNodeValue.CompareTo(item)) > 0)
+            {
+                if (this.LeftTree == null)
+                    this.LeftTree = new BinarySearchTree<T>(item);
+                else
+                    this.LeftTree.Insert(item);
+            }
+            else
+            {
+                if (this.RightTree == null)
+                {
+                    this.RightTree = new BinarySearchTree<T>(item);
+                }
+                else
+                    this.RightTree.Insert(item);
+            }
+        }
+
+        public void Display()
+        {
+            if(this.LeftTree != null)
+            {
+                this.leftCount++;
+                this.LeftTree.Display();
+            }
+            Console.WriteLine(this.NodeData.ToString());
+            if(this.RightTree != null)
+            {
+                this.rightCount++;
+                this.RightTree.Display();  
+            }
+        }
+
+        
+
+        
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿namespace BinarySearchTree_Day15
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World");
+            BinarySearchTree<int> binarySearch = new BinarySearchTree<int>(56);
+
+            binarySearch.Insert(30);
+            binarySearch.Insert(70);
+            binarySearch.Display();
+        }
+    }
+}
